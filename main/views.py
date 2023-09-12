4b6e09b2dbddc2555b1741c7ab4e5fc0516e3699
Ganeshdarshan <bhatganeshdarshan10@gmail.com>
2023-09-12 11:18:46 +0530

    disabled other users from deleting others posts

--- main/views.py
+++ main/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render ,redirect
+from django.http import HttpResponse
 from .models import Room,Topic
 from .forms import RoomForm
 from django.db.models import Q
@@ -65,6 +66,10 @@ def createRoom(request):
 def updateRoom(request,pk):
     room= Room.objects.get(id=pk)
     form= RoomForm(instance=room)
+
+    if request.user != room.host:
+        return HttpResponse("You are not allowed here !! ")
+
     if request.method == 'POST':
         form = RoomForm(request.POST,instance=room)
         if form.is_valid():
